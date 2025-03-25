f4126c20280f12065543ad2b1513304476ecddc8
enigmaDevelopments <enigmarezac@gmail.com>
2025-03-25 17:55:15 -0400

    added floor sprite to leaf nodes of tree

--- Assets/QuadTree.cs
+++ Assets/QuadTree.cs
@@ -2,19 +2,23 @@ using UnityEngine;
 
 public class QuadTree : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject tile;
     void Start()
     {
-        MakeTree(transform,512);
+        MakeTree(transform,128);
     }
 
     void MakeTree(Transform current, int size)
     {
-        if (size == 4)
+        if (size == 2)
             return;
         for(byte i = 0; i < 4; i++)
         {
-            GameObject child = new GameObject("child");
+            GameObject child;
+            if (size == 4)
+                child = Instantiate(tile);
+            else
+                child = new GameObject("child");
             child.transform.SetParent(current);
             child.transform.localPosition = new Vector3(size / 4 * ((i%2*2)-1), size / 4 * ((i/2*2) - 1));
             MakeTree(child.transform, size / 2);
