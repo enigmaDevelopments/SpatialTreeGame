081e134ce1ef61b63851dbe4c9155d4453422b28
enigmaDevelopments <enigmarezac@gmail.com>
2025-03-27 14:29:24 -0400

    edited local variables to improve gameplay

--- Assets/scripts/SpawnZombie.cs
+++ Assets/scripts/SpawnZombie.cs
@@ -4,17 +4,26 @@ public class SpawnZombie : MonoBehaviour
 {
     public GameObject zombie;
     public Transform player;
+    public Transform camera;
     public QuadTree tree;
     public float spawnRate = .01f;
+    public float playerDistance = 3;
     void FixedUpdate()
+    {
+        Spwan();
+        Spwan();
+        Spwan();
+        Spwan();
+    }
+    private void Spwan()
     {
         if (Random.value < spawnRate)
         {
             Vector3 pos;
             do
             {
-               pos = new Vector3(Random.Range(-tree.size, tree.size), Random.Range(-tree.size, tree.size), -.4f);
-            } while (Vector3.Distance(pos, player.position) < 3);
+                pos = new Vector3(Random.Range(-tree.size, tree.size), Random.Range(-tree.size, tree.size), -.4f);
+            } while (Vector3.Distance(pos, camera.position) < playerDistance);
             GameObject zombieInstance = Instantiate(zombie, pos, Quaternion.identity);
             zombieInstance.GetComponent<FollowPlayer>().target = player;
             zombieInstance.GetComponent<TreeInterface>().tree = tree;
